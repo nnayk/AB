76fa85801b0fef50a82a593759cd64d87d585c32
nnayk <79224511+nnayk@users.noreply.github.com>
2022-03-06 08:53:30 -0800

    web app w frontend and backend

--- Scrape/routes.py
+++ Scrape/routes.py
@@ -5,6 +5,7 @@ from Scrape.forms import RegisterForm, LoginForm, FilterForm
 from Scrape.models import Item, User
 from Scrape import db
 from Scrape.General import General
+import sqlite3
 from flask_login import login_user, logout_user, login_required
 
 
@@ -31,8 +32,6 @@ def dealsPage():
 
     if request.method == "POST":
         productEntered = request.form.get("search", "")
-        saveString = ''
-        saveItems = []
         shipBox = request.form.getlist('freeShipping')
         if 'on' in shipBox:
             # print('applu')
@@ -47,6 +46,9 @@ def dealsPage():
         # print("savey=",saveString)
         if productEntered == '':
             return render_template("deals.html", jiggle=jiggle, items='', titles='', saveString=saveString, saveItems=saveItems)
+        else:
+            saveString = ''
+            saveItems = []
         sort_by = jiggle.filterMode.data
 
         if sort_by == "rating":
@@ -61,22 +63,22 @@ def dealsPage():
         products_list = storeObj.startScrape(
             productEntered, shipChecked, condChecked, sort_by)
         items = products_list
-    # print("savestringa",saveString)
+    #print("savestringa", saveString)
     # return render_template('lillu.html')
     return render_template('deals.html', jiggle=jiggle, items=items, titles=titles, saveString=saveString, saveItems=saveItems)
 
 
-@app.route("/register", methods=["GET", "POST"])
+@ app.route("/register", methods=["GET", "POST"])
 def registerPage():
     form = RegisterForm()
     if form.validate_on_submit():
-        userToCreate = User(username=form.username.data, email=form.email.data,
+        userToCreate = User(username=form.username.data,
                             password=form.pwd1.data)
         db.session.add(userToCreate)
         db.session.commit()
         login_user(userToCreate)
         flash(f"Welcome, {userToCreate.username}", category='success')
-        return redirect(url_for('dealsPage'))
+        return redirect(url_for('preferencePage'))
     if form.errors != {}:
         for error_msg in form.errors.values():
             if ' '.join(error_msg) == 'Field must be equal to pwd1.':
@@ -110,3 +112,43 @@ def logoutPage():
     logout_user()
     flash("Successfully logged out. We hope to see you again.", category="info")
     return redirect(url_for("homePage"))
+
+
+@app.route("/account")
+def accountPage():
+    return render_template('account.html')
+
+
+@app.route("/preferences")
+def preferencePage():
+    return render_template('preference.html')
+
+
+@app.route("/groupPage")
+# event,desc,time,location,register
+def groupPage():
+    conn = sqlite3.connect("market.db")
+    c = conn.cursor()
+    # upcoming_events = ["Meet and Greet", "Sunday Breakfast"]
+    # descs = ["Meets every 2 weeks.\nCome meet other veterans\nin your area, and connect with industry professionals!\nAttend for as long as you wish.",
+    #          "Enjoy a hearty breakfast with other veterans!"]
+    # time = ["5:00PM-7:00PM", "8:30AM-10:30AM"]
+    # location = ["SLO Health Center: 1495 Palm St,San Luis Obispo,\nCA 93401",
+    #             "Veteran's Hall\n801 Grand Ave, San Luis Obispo,\nCA 93401"]
+
+    # for i in range(2):
+    #     c.execute("INSERT INTO events VALUES (?,?,?,?)",
+    #               (upcoming_events[i], descs[i], time[i], location[i]))
+    # conn.commit()
+    data = []
+    rows = c.execute("SELECT * FROM events")
+
+    for row in rows:
+        data.append(row)
+
+    return render_template('groupPage.html', titles=["Event Name", "Description", "Time", "Location", "Register"], info=data)
+
+
+@app.route("/soloPage")
+def soloPage():
+    return render_template('soloPage.html')
